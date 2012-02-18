27d4986b11fb99cb3448432bfb5e9469b728fb11
Cameron Neylon <cn@cameronneylon.net>
2012-02-18 16:49:21 +0000

    corrected typo in setup.py

--- setup.py
+++ setup.py
@@ -11,11 +11,11 @@ setup(name='pycml',
       author='Cameron Neylon',
       author_email='pypi@cameroneylon.net',
       url='https://github.com/cameronneylon/pycml',
-      packages=find_packages(exclude=['test'])
+      packages=find_packages(exclude=['test']),
       install_requires = [
           'xml.etree.ElementTree',
           'warnings',
           'numpy'
-          ]
+          ],
       test_suite='test'
      )
